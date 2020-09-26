ed820fba11b5ba040080d08b758ade4417bf4910
syxxcalypso <jen.sanchez1404@gmail.com>
2020-09-26 00:29:33 +0000

    initial

---
+++ sender.py
@@ -0,0 +1,89 @@
+import socket
+import sys
+# import _thread
+import time
+import string
+import packet
+import udt
+import random
+from timer import Timer
+
+# Some already defined parameters
+PACKET_SIZE = 512
+RECEIVER_ADDR = ('localhost', 8080)
+SENDER_ADDR = ('localhost', 9090)
+SLEEP_INTERVAL = 0.05 # (In seconds)
+TIMEOUT_INTERVAL = 0.5
+WINDOW_SIZE = 4
+
+# You can use some shared resources over the two threads
+# base = 0
+# mutex = _thread.allocate_lock()
+# timer = Timer(TIMEOUT_INTERVAL)
+
+# Need to have two threads: one for sending and another for receiving ACKs
+
+# Generate random payload of any length
+def generate_payload(length=10):
+    letters = string.ascii_lowercase
+    result_str = ''.join(random.choice(letters) for i in range(length))
+
+    return result_str
+
+
+# Send using Stop_n_wait protocol
+def send_snw(sock):
+    # Fill out the code here
+    seq = 0
+    while(seq < 20):
+        data = generate_payload(40).encode()
+        pkt = packet.make(seq, data)
+        print("Sending seq# ", seq, "\n")
+        udt.send(pkt, sock, RECEIVER_ADDR)
+        seq = seq+1
+        time.sleep(TIMEOUT_INTERVAL)
+    pkt = packet.make(seq, "END".encode())
+    udt.send(pkt, sock, RECEIVER_ADDR)
+
+# Send using GBN protocol
+def send_gbn(sock):
+
+    return
+
+# Receive thread for stop-n-wait
+def receive_snw(sock, pkt):
+    # Fill here to handle acks
+    timer = Timer(TIMEOUT_INTERVAL)
+    timer.start() # UDP Packets should be non-blocking
+    while not timer.timeout():
+        _pkt, recvaddr = udt.recv(sock)
+        seq, data = packet.extract(_pkt)
+        if data:
+            return # Receiver should only be sending ACK
+        else:
+            udt.send(pkt, sock, RECEIVER_ADDR)
+            timer.stop()
+            timer.start()
+   
+
+# Receive thread for GBN
+def receive_gbn(sock):
+    # Fill here to handle acks
+    return
+
+
+# Main function
+if __name__ == '__main__':
+    # if len(sys.argv) != 2:
+    #     print('Expected filename as command line argument')
+    #     exit()
+
+    sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    sock.bind(SENDER_ADDR)
+
+    # filename = sys.argv[1]
+
+    send_snw(sock)
+    sock.close()
+
+
