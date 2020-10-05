61e6787d1a73995d765ad15b345994154c3ea794
syxxcalypso <jen.sanchez1404@gmail.com>
2020-10-05 13:53:46 -0600

    Document receiver end of receive_snw

--- receiver.py
+++ receiver.py
@@ -20,20 +20,40 @@ def receive_sr(sock, windowsize):
 
 # Receive packets from the sender w/ Stop-n-wait protocol
 def receive_snw(sock):
+
+    # Terminal String
     endStr = ''
+
+    # Most recent sequence number
     _seq = -1
+
+    # Blocking Loop
     while True:
+
+        # Block on socket data
         pkt, senderaddr = udt.recv(sock)
         seq, data = packet.extract(pkt)
+
+        # If data is newer
         if _seq != seq:
+
+            # Update last sequence
             _seq = seq
+
+            # Parse data and write debugging info to logging stream
             endStr = data.decode()
             sys.stderr.write("From: {}, Seq# {}\n".format(senderaddr, seq))
             sys.stderr.flush()
+
+            # If string is terminal
             if endStr == 'END':
                 return
+
+            # Write socket data to output stream
             sys.stdout.write(endStr)
             sys.stdout.flush()
+
+        # Send null ACK
         udt.send(b' ', sock, ('localhost', 9090))
 
 
